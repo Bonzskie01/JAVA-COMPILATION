0c5f7501dbf2d3676e760ab42ddae09e8e886e17
RTC CODES <150908947+Bonzskie01@users.noreply.github.com>
2024-10-15 10:35:07 +0800

    Update Info.java

--- Info.java
+++ Info.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Info {
 	public static void main (String[] args){
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.print("Enter Your Name: ");
 		String nameInput = scanner.nextLine();
 		System.out.print("Enter Your Address: ");
